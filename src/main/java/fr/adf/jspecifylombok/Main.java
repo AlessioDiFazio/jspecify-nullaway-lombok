60ac0a11713547c9f42722ee321fdd2c8dcb088b
Alessio Di Fazio <alessio.difazio@doctolib.com>
2025-09-18 11:20:48 +0200

    more examples
    Signed-off-by: Alessio Di Fazio <alessio.difazio@doctolib.com>

--- src/main/java/fr/adf/jspecifylombok/Main.java
+++ src/main/java/fr/adf/jspecifylombok/Main.java
@@ -1,11 +1,19 @@
 package fr.adf.jspecifylombok;
 
 import fr.adf.jspecifylombok.records.RecordWithBuilder;
+import java.util.Locale;
+import org.jspecify.annotations.Nullable;
 
 public class Main {
     public static void main(String[] args) {
-        var recordWithBuilder =
-                RecordWithBuilder.builder().arg1("hello").arg2(null).build();
+        var recordWithBuilder = RecordWithBuilder.builder().arg1("hello").build();
         System.out.println("recordWithBuilder = " + recordWithBuilder);
+
+        var main = new Main();
+        main.doSomethingWithArg(recordWithBuilder.arg2());
+    }
+
+    public void doSomethingWithArg(@Nullable String arg) {
+        System.out.println("doSomethingWithArg: " + String.valueOf(arg).toUpperCase(Locale.ROOT));
     }
 }
